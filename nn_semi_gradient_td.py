21a672c804f1073e9cca51fc89733c28711552c6
Joe Cipolla <jlcipolla@gmail.com>
2019-11-21 17:28:40 -0600

    unit tests

--- nn_semi_gradient_td.py
+++ nn_semi_gradient_td.py
@@ -479,204 +479,12 @@ def agent_message(self, message):
         return state_value
 
 
-# ---------------------------------------------------------------------------------------------------------------------#
-# run experiment
-# 1. plot learned state value function and compare it against the true state values
-# 2. plot learning curve depicting the error in the learned value estimates over episodes
-#
-# prediction objective is root mean squared value error (rmsve)
-# rmsve = sum[ mu_s * (v_pi_s - v_hat(s, w)]^2
-
-# Here we provide you with the true state value and state distribution
-true_state_val = np.load('data/true_V.npy')
-state_distribution = np.load('data/state_distribution.npy')
-
-def calc_RMSVE(learned_state_val):
-    assert(len(true_state_val) == len(learned_state_val) == len(state_distribution))
-    MSVE = np.sum(np.multiply(state_distribution, np.square(true_state_val - learned_state_val)))
-    RMSVE = np.sqrt(MSVE)
-    return RMSVE
-
-# Define function to run experiment
-def run_experiment(environment, agent, environment_parameters, agent_parameters, experiment_parameters):
-    rl_glue = RLGlue(environment, agent)
-
-    # Sweep Agent parameters
-    for num_agg_states in agent_parameters["num_groups"]:
-        for step_size in agent_parameters["step_size"]:
-
-            # save rmsve at the end of each evaluation episode
-            # size: num_episode / episode_eval_frequency + 1 (includes evaluation at the beginning of training)
-            agent_rmsve = np.zeros(
-                int(experiment_parameters["num_episodes"] / experiment_parameters["episode_eval_frequency"]) + 1)
-
-            # save learned state value at the end of each run
-            agent_state_val = np.zeros(environment_parameters["num_states"])
-
-            env_info = {"num_states": environment_parameters["num_states"],
-                        "start_state": environment_parameters["start_state"],
-                        "left_terminal_state": environment_parameters["left_terminal_state"],
-                        "right_terminal_state": environment_parameters["right_terminal_state"]}
-
-            agent_info = {"num_states": environment_parameters["num_states"],
-                          "num_groups": num_agg_states,
-                          "step_size": step_size,
-                          "discount_factor": environment_parameters["discount_factor"]}
-
-            print('Setting - num. agg. states: {}, step_size: {}'.format(num_agg_states, step_size))
-            os.system('sleep 0.2')
-
-            # one agent setting
-            for run in tqdm(range(1, experiment_parameters["num_runs"] + 1)):
-                env_info["seed"] = run
-                agent_info["seed"] = run
-                rl_glue.rl_init(agent_info, env_info)
-
-                # Compute initial RMSVE before training
-                current_V = rl_glue.rl_agent_message("get state value")
-                agent_rmsve[0] += calc_RMSVE(current_V)
-
-                for episode in range(1, experiment_parameters["num_episodes"] + 1):
-                    # run episode
-                    rl_glue.rl_episode(0)  # no step limit
-
-                    if episode % experiment_parameters["episode_eval_frequency"] == 0:
-                        current_V = rl_glue.rl_agent_message("get state value")
-                        agent_rmsve[int(episode / experiment_parameters["episode_eval_frequency"])] += calc_RMSVE(
-                            current_V)
-
-                # store only one run of state value
-                if run == 50:
-                    agent_state_val = rl_glue.rl_agent_message("get state value")
-
-            # rmsve averaged over runs
-            agent_rmsve /= experiment_parameters["num_runs"]
-
-            save_name = "{}_agg_states_{}_step_size_{}".format('TD_agent', num_agg_states, step_size).replace('.', '')
-
-            if not os.path.exists('results'):
-                os.makedirs('results')
-
-            # save avg. state value
-            np.save("results/V_{}".format(save_name), agent_state_val)
-
-            # save avg. rmsve
-            np.save("results/RMSVE_{}".format(save_name), agent_rmsve)
-
-
-# Run Experiment
-
-# Experiment parameters
-experiment_parameters = {
-    "num_runs" : 50,
-    "num_episodes" : 2000,
-    "episode_eval_frequency" : 10 # evaluate every 10 episodes
-}
-
-# Environment parameters
-environment_parameters = {
-    "num_states" : 500,
-    "start_state" : 250,
-    "left_terminal_state" : 0,
-    "right_terminal_state" : 501,
-    "discount_factor" : 1.0
-}
-
-# Agent parameters
-# Each element is an array because we will be later sweeping over multiple values
-agent_parameters = {
-    "num_groups": [10],
-    "step_size": [0.01, 0.05, 0.1]
-}
-
-current_env = RandomWalkEnvironment
-current_agent = TDAgent
-
-run_experiment(current_env, current_agent, environment_parameters, agent_parameters, experiment_parameters)
-plot_script.plot_result(agent_parameters, 'results')
-
-
-# Run experiment with different state aggregation resolution and step-size
-#
-# we will test several values of num_groups and step_size. Parameter sweeps although necessary, can take lots of time.
-# So now that you have verified your experiment result, here we show you the results of the parameter sweeps that you
-# would see when running the sweeps yourself.
-#
-# We tested several different values of num_groups: {10, 100, 500}, and step-size: {0.01, 0.05, 0.1}.
-# As before, we performed 2000 episodes per run, and averaged the results over 50 runs for each setting.
-
-# Experiment parameters
-experiment_parameters = {
-    "num_runs" : 50,
-    "num_episodes" : 2000,
-    "episode_eval_frequency" : 10 # evaluate every 10 episodes
-}
-
-# Environment parameters
-environment_parameters = {
-    "num_states" : 500,
-    "start_state" : 250,
-    "left_terminal_state" : 0,
-    "right_terminal_state" : 501,
-    "discount_factor" : 1.0
-}
-
-# Agent parameters
-# Each element is an array because we will be sweeping over multiple values
-agent_parameters = {
-    "num_groups": [10, 100, 500],
-    "step_size": [0.01, 0.05, 0.1]
-}
-
-if all_correct:
-    plot_script.plot_result(agent_parameters, 'correct_npy')
-else:
-    raise ValueError("Make sure your experiment result is correct! Otherwise the sweep results will not be displayed.")
 
 
 # ---------------------------------------------------------------------------------------------------------------------#
 # unit tests
+## Test Code for get_value() ##
 
-# Test Code for agent_policy()
-test_rand_generator = np.random.RandomState(99)
-state = 250
-action_array = []
-for i in range(10):
-    action_array.append(agent_policy(test_rand_generator, 250))
-print('action_array: {}'.format(action_array))
-
-# Test Code for get_state_feature()
-# Given that num_states = 10 and num_groups = 5, test get_state_feature()
-# There are states 1~10, and the state feature vector would be of size 5.
-# Only one element would be active for any state feature vector.
-# get_state_feature() should support various values of num_states, num_groups, not just this example
-# For simplicity, assume num_states will always be perfectly divisible by num_groups
-num_states = 10
-num_groups = 5
-num_states_in_group = int(num_states / num_groups)
-
-# Test 1st group, state = 1
-state = 1
-print("1st group: {}".format(get_state_feature(num_states_in_group, num_groups, state)))
-
-# Test 2nd group, state = 3
-state = 3
-print("2nd group: {}".format(get_state_feature(num_states_in_group, num_groups, state)))
-
-# Test 3rd group, state = 6
-state = 6
-print("3rd group: {}".format(get_state_feature(num_states_in_group, num_groups, state)))
-
-# Test 4th group, state = 7
-state = 7
-print("4th group: {}".format(get_state_feature(num_states_in_group, num_groups, state)))
-
-# Test 5th group, state = 10
-state = 10
-print("5th group: {}".format(get_state_feature(num_states_in_group, num_groups, state)))
-
-
-# Test Code for get_value()
 # Suppose num_states = 5, num_hidden_layer = 1, and num_hidden_units = 10
 num_hidden_layer = 1
 s = np.array([[0, 0, 0, 1, 0]])
@@ -695,115 +503,71 @@ assert(np.allclose(estimated_value, np.array([[-0.21915705]])))
 print ("Passed the assert!")
 
 
-# Test Code for agent_init()
-agent_info = {"num_states": 500,
-              "num_groups": 10,
-              "step_size": 0.1,
-              "discount_factor": 1.0,
-              "seed": 1}
+## Test Code for get_gradient() ##
 
-test_agent = TDAgent()
-test_agent.agent_init(agent_info)
+# Suppose num_states = 5, num_hidden_layer = 1, and num_hidden_units = 2
+num_hidden_layer = 1
+s = np.array([[0, 0, 0, 1, 0]])
 
-# check attributes
-print("num_states: {}".format(test_agent.num_states))
-print("num_groups: {}".format(test_agent.num_groups))
-print("step_size: {}".format(test_agent.step_size))
-print("discount_factor: {}".format(test_agent.discount_factor))
+weights_data = np.load("asserts/get_gradient_weights.npz")
+weights = [dict() for i in range(num_hidden_layer+1)]
+weights[0]["W"] = weights_data["W0"]
+weights[0]["b"] = weights_data["b0"]
+weights[1]["W"] = weights_data["W1"]
+weights[1]["b"] = weights_data["b1"]
 
-print("weights shape: {}".format(test_agent.weights.shape))
-print("weights init. value: {}".format(test_agent.weights))
+grads = get_gradient(s, weights)
 
+grads_answer = np.load("asserts/get_gradient_grads.npz")
 
-# Test Code for agent_start() and agent_policy()
-agent_info = {"num_states": 500,
-              "num_groups": 10,
-              "step_size": 0.1,
-              "discount_factor": 1.0,
-              "seed": 1
-             }
-state = 250
-test_agent = TDAgent()
-test_agent.agent_init(agent_info)
-test_agent.agent_start(state)
+print("grads[0][\"W\"]\n", grads[0]["W"], "\n")
+print("grads[0][\"b\"]\n", grads[0]["b"], "\n")
+print("grads[1][\"W\"]\n", grads[1]["W"], "\n")
+print("grads[1][\"b\"]\n", grads[1]["b"], "\n")
 
-print("Agent state: {}".format(test_agent.last_state))
-print("Agent selected action: {}".format(test_agent.last_action))
+assert(np.allclose(grads[0]["W"], grads_answer["W0"]))
+assert(np.allclose(grads[0]["b"], grads_answer["b0"]))
+assert(np.allclose(grads[1]["W"], grads_answer["W1"]))
+assert(np.allclose(grads[1]["b"], grads_answer["b1"]))
+
+print("Passed the asserts!")
 
 
-# Test Code for agent_step()
-# Make sure agent_init() and agent_start() are working correctly first.
-# agent_step() should work correctly for other arbitrary state transitions in addition to this test case.
-agent_info = {"num_states": 500,
-              "num_groups": 10,
-              "step_size": 0.1,
-              "discount_factor": 0.9,
-              "seed": 1}
-test_agent = TDAgent()
-test_agent.agent_init(agent_info)
+## Test Code for update_weights() ##
 
-# Initializing the weights to arbitrary values to verify the correctness of weight update
-test_agent.weights = np.array([-1.5, 0.5, 1., -0.5, 1.5, -0.5, 1.5, 0.0, -0.5, -1.0])
-print("Initial weights: {}".format(test_agent.weights))
+# Suppose num_states = 5, num_hidden_layer = 1, and num_hidden_units = 2
+num_hidden_layer = 1
 
-# Assume the agent started at State 50
-start_state = 50
-action = test_agent.agent_start(start_state)
+weights_data = np.load("asserts/update_weights_weights.npz")
+weights = [dict() for i in range(num_hidden_layer+1)]
+weights[0]["W"] = weights_data["W0"]
+weights[0]["b"] = weights_data["b0"]
+weights[1]["W"] = weights_data["W1"]
+weights[1]["b"] = weights_data["b1"]
 
-# Assume the reward was 10.0 and the next state observed was State 120
-reward = 10.0
-next_state = 120
-test_agent.agent_step(reward, next_state)
-print("Updated weights: {}".format(test_agent.weights))
+g_data = np.load("asserts/update_weights_g.npz")
+g = [dict() for i in range(num_hidden_layer+1)]
+g[0]["W"] = g_data["W0"]
+g[0]["b"] = g_data["b0"]
+g[1]["W"] = g_data["W1"]
+g[1]["b"] = g_data["b1"]
 
-if np.allclose(test_agent.weights, np.array([-0.26, 0.5, 1., -0.5, 1.5, -0.5, 1.5, 0., -0.5, -1.])):
-    print("weight update is correct!\n")
-else:
-    print("weight update is incorrect.\n")
+test_sgd = SGD()
+optimizer_info = {"step_size": 0.3}
+test_sgd.optimizer_init(optimizer_info)
+updated_weights = test_sgd.update_weights(weights, g)
 
-print("last state: {}".format(test_agent.last_state))
-print("last action: {}".format(test_agent.last_action))
+# updated weights asserts
+updated_weights_answer = np.load("asserts/update_weights_updated_weights.npz")
 
+print("updated_weights[0][\"W\"]\n", updated_weights[0]["W"], "\n")
+print("updated_weights[0][\"b\"]\n", updated_weights[0]["b"], "\n")
+print("updated_weights[1][\"W\"]\n", updated_weights[1]["W"], "\n")
+print("updated_weights[1][\"b\"]\n", updated_weights[1]["b"], "\n")
 
-# Test Code for agent_end()
-# Make sure agent_init() and agent_start() are working correctly first.
+assert(np.allclose(updated_weights[0]["W"], updated_weights_answer["W0"]))
+assert(np.allclose(updated_weights[0]["b"], updated_weights_answer["b0"]))
+assert(np.allclose(updated_weights[1]["W"], updated_weights_answer["W1"]))
+assert(np.allclose(updated_weights[1]["b"], updated_weights_answer["b1"]))
 
-agent_info = {"num_states": 500,
-              "num_groups": 10,
-              "step_size": 0.1,
-              "discount_factor": 0.9,
-              "seed": 1}
-
-test_agent = TDAgent()
-test_agent.agent_init(agent_info)
-
-# Initializing the weights to arbitrary values to verify the correctness of weight update
-test_agent.weights = np.array([-1.5, 0.5, 1., -0.5, 1.5, -0.5, 1.5, 0.0, -0.5, -1.0])
-print("Initial weights: {}".format(test_agent.weights))
-
-# Assume the agent started at State 50
-start_state = 50
-test_agent.agent_start(start_state)
-
-# Assume the reward was 10.0 and reached the terminal state
-test_agent.agent_end(10.0)
-print("Updated weights: {}".format(test_agent.weights))
-
-if np.allclose(test_agent.weights, np.array([-0.35, 0.5, 1., -0.5, 1.5, -0.5, 1.5, 0., -0.5, -1.])):
-    print("weight update is correct!\n")
-else:
-    print("weight update is incorrect.\n")
-
-
-# Test Code for agent_get_state_val()
-agent_info = {"num_states": 20,
-              "num_groups": 5,
-              "step_size": 0.1,
-              "discount_factor": 1.0}
-
-test_agent = TDAgent()
-test_agent.agent_init(agent_info)
-test_state_val = test_agent.agent_message('get state value')
-
-print("State value shape: {}".format(test_state_val.shape))
-print("Initial State value for all states: {}".format(test_state_val))
+print("Passed the asserts!")
