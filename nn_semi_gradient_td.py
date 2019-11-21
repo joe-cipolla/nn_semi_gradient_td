ed44cc4127e2c260c3ce47925b138de602304acd
Joe Cipolla <jlcipolla@gmail.com>
2019-11-21 17:05:06 -0600

    adam optimizer

--- nn_semi_gradient_td.py
+++ nn_semi_gradient_td.py
@@ -214,7 +214,7 @@ def get_gradient(s, weights):
 
 
 # ---------------------------------------------------------------------------------------------------------------------#
-# Implement stochastic gradient descent method for state-value prediction
+# stochastic gradient descent method for state-value prediction
 #
 # At each time step, we update the weights in the direction g_t = D_t * Del * v_hat(S_t, w_t) using a fixed setp-size alpha.
 # D_t = R_t+1 + gamma * v_hat(S_t+1, w_t) - v_hat(S_t, w_t) is the TD-error.
@@ -249,6 +249,101 @@ class SGD(BaseOptimizer):
                 weights[i][param] += self.step_size * g[i][param]
 
         return weights
+
+
+# ---------------------------------------------------------------------------------------------------------------------#
+# Adam Algoritmn
+#
+# Instead of using SGD for updating the weights, we use a more advanced algorithm called Adam.
+# The Adam algorithm improves the SGD update with two concepts: adaptive vecotr step-sizes and momentum.
+# It keeps estimates of the mean and second moment of the updates, denoted by m and v respectively:
+# m_t = beta_m * m_t-1 + (1 - beta_m) * g_t
+# v_t = beta_v * v_t-1 + (1 - beta_v) * (g_t)^2
+#
+# Given that m and v are initialized to zero, they are biased toward zero. To get unbiased estiamtes of the mean and
+# second moment, Adam defines m_hat and v_hat as:
+# m_t_hat = m_t / (1 - beta_t_m)
+# v_t_hat = v_t / (1 - beta_t_v)
+# The weights are then updated as follows:
+# w_t = w_t-1 + ( alpha / (sqrt(v_t_hat) + epsilon) ) * m_t_hat
+#
+# When implmenting the agent we use the Adam algorithm instead of SGD because it is more efficient.
+
+class Adam(BaseOptimizer):
+    def __init__(self):
+        pass
+
+    def optimizer_init(self, optimizer_info):
+        """Setup for the optimizer.
+
+        Set parameters needed to setup the Adam algorithm.
+
+        Assume optimizer_info dict contains:
+        {
+            num_states: integer,
+            num_hidden_layer: integer,
+            num_hidden_units: integer,
+            step_size: float,
+            self.beta_m: float
+            self.beta_v: float
+            self.epsilon: float
+        }
+        """
+
+        self.num_states = optimizer_info.get("num_states")
+        self.num_hidden_layer = optimizer_info.get("num_hidden_layer")
+        self.num_hidden_units = optimizer_info.get("num_hidden_units")
+
+        # Specify Adam algorithm's hyper parameters
+        self.step_size = optimizer_info.get("step_size")
+        self.beta_m = optimizer_info.get("beta_m")
+        self.beta_v = optimizer_info.get("beta_v")
+        self.epsilon = optimizer_info.get("epsilon")
+
+        self.layer_size = np.array([self.num_states, self.num_hidden_units, 1])
+
+        # Initialize Adam algorithm's m and v
+        self.m = [dict() for i in range(self.num_hidden_layer+1)]
+        self.v = [dict() for i in range(self.num_hidden_layer+1)]
+
+        for i in range(self.num_hidden_layer+1):
+
+            # Initialize self.m[i]["W"], self.m[i]["b"], self.v[i]["W"], self.v[i]["b"] to zero
+            self.m[i]["W"] = np.zeros((self.layer_size[i], self.layer_size[i+1]))
+            self.m[i]["b"] = np.zeros((1, self.layer_size[i+1]))
+            self.v[i]["W"] = np.zeros((self.layer_size[i], self.layer_size[i+1]))
+            self.v[i]["b"] = np.zeros((1, self.layer_size[i+1]))
+
+        # Initialize beta_m_product and beta_v_product to be later used for computing m_hat and v_hat
+        self.beta_m_product = self.beta_m
+        self.beta_v_product = self.beta_v
+
+    def update_weights(self, weights, g):
+        """
+        Given weights and update g, return updated weights
+        """
+
+        for i in range(len(weights)):
+            for param in weights[i].keys():
+
+                ### update self.m and self.v
+                self.m[i][param] = self.beta_m * self.m[i][param] + (1 - self.beta_m) * g[i][param]
+                self.v[i][param] = self.beta_v * self.v[i][param] + (1 - self.beta_v) * (g[i][param] * g[i][param])
+
+                ### compute m_hat and v_hat
+                m_hat = self.m[i][param] / (1 - self.beta_m_product)
+                v_hat = self.v[i][param] / (1 - self.beta_v_product)
+
+                ### update weights
+                weights[i][param] += self.step_size * m_hat / (np.sqrt(v_hat) + self.epsilon)
+
+        ### update self.beta_m_product and self.beta_v_product
+        self.beta_m_product *= self.beta_m
+        self.beta_v_product *= self.beta_v
+
+        return weights
+
+
 
 # ---------------------------------------------------------------------------------------------------------------------#
 # Implement agent methods
